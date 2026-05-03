5a6710f4383bb574f2959f1d73ceab0461f3f44a
HARIRAJESH014 <harirajesh.c@gmail.com>
2026-05-03 10:52:35 +0530

    Create program4.cpp

---
+++ program4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class A {
+    int x = 10;
+    friend void show(A);
+};
+
+void show(A a) {
+    cout << a.x;
+}
+
+int main() {
+    A a;
+    show(a);
+}
